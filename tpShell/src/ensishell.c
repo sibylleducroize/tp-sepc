57d9926fa0e8aa1c7bebf02f70dfb2c9edd901a5
Juliette Louistisserand <juliette.louistisserand@grenoble-inp.org>
2023-03-28 17:44:43 +0200

    Jobs marche, tâche de fond aussi

--- tpShell/src/ensishell.c
+++ tpShell/src/ensishell.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include "variante.h"
 #include "readcmd.h"
@@ -58,21 +60,77 @@ void terminate(char *line) {
 	exit(0);
 }
 
+struct Processus {
+	pid_t pid;
+	char * name;
+	struct Processus * next;
+};
+
+void ajout_processus(pid_t pid, char* name, struct Processus ** liste) {
+	/*ajout d'un processus à la liste des processus lancés en tâche de fond*/
+	struct Processus * p = (struct Processus *) malloc(sizeof(struct Processus));
+	p->pid = pid;
+	p->name = strdup(name);
+	p->next = *liste;
+
+	*liste = p;
+}
+
+void grand_menage_processus(struct Processus ** liste) {
+	/*supprime tous les processus terminés de la liste*/
+	struct Processus * prec = NULL;
+	struct Processus * courant = * liste;
+
+	while(courant != NULL) {
+		int32_t status;
+		pid_t pid = waitpid(courant->pid, &status, WNOHANG);
+
+		if(pid != 0) {
+			/*a terminé : on supprime*/
+			prec->next = courant->next;
+			free(courant->name);
+			free(courant);
+			/*on continue le parcours*/
+			courant = prec->next;
+		}
+		else {
+			/*on continue le parcours*/
+			prec = courant;
+			courant = courant->next;
+		}
+	}
+}
+
+void jobs(struct Processus ** liste_processus) {
+	/*afficher la liste des processus lancés en tâche de fond de votre shell
+	avec leur pid et la commande lancée*/
+	grand_menage_processus(liste_processus);
+
+	struct Processus *p = *liste_processus;
+    while(p != NULL) {
+        printf("[%d] running %s\n", p->pid, p->name);
+        p = p->next;
+    }
+}
+
 
 int main() {
-        printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
+	
+	struct Processus * liste_processus = NULL;
+
+    printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
 #if USE_GUILE == 1
-        scm_init_guile();
-        /* register "executer" function in scheme */
-        scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
+    scm_init_guile();
+    /* register "executer" function in scheme */
+    scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
 #endif
 
 	while (1) {
 		struct cmdline *l;
 		char *line=0;
-		int i, j;
 		char *prompt = "ensishell>";
+		int status;
 
 		/* Readline use some internal memory structure that
 		   can not be cleaned at the end of the program. Thus
@@ -94,39 +152,61 @@ int main() {
 			sprintf(catchligne, "(catch #t (lambda () %s) (lambda (key . parameters) (display \"mauvaise expression/bug en scheme\n\")))", line);
 			scm_eval_string(scm_from_locale_string(catchligne));
 			free(line);
-                        continue;
-                }
+            continue;
+        }
 #endif
-
 		/* parsecmd free line and set it up to 0 */
-		l = parsecmd( & line);
+		l = parsecmd(& line);
 
 		/* If input stream closed, normal termination */
 		if (!l) {
-		  
 			terminate(0);
 		}
-		
 
-		
 		if (l->err) {
 			/* Syntax error, read another command */
 			printf("error: %s\n", l->err);
 			continue;
 		}
 
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
+		/*exécution des commandes*/
+		for (uint32_t i=0; l->seq[i]!=0; i++) {
 
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
+			if (!strncmp(l->seq[0][0], "jobs", 4)) {
+				/*commande interne jobs pour afficher les processus en arrière plan*/
+				jobs(&liste_processus);
+				continue;
+			}
+
+			/*fork pour dupliquer le processus*/
+			pid_t pid = fork();
+
+			if (pid == -1) {
+				printf("erreur de fork");
+			}
+
+			/*processus enfant*/
+			else if (pid == 0) {
+				/*execvp pour avoir le bon processus*/
+				execvp(l->seq[i][0], l->seq[i]);
+				exit(0);
+			}
+
+			/*processus parent*/
+			else {
+				if(!l->bg) {
+					/*processus pas en tâche de fond => le père attend la fin du fils*/
+					pid_t wpid = waitpid(pid, &status, 0);
+					if(wpid == -1) {
+						printf("erreur dans le wait");
+						exit(1);
+					}
+				}
+				else {
+					/*processus fils en tâche de fond : onl'ajoute à la liste*/
+					ajout_processus(pid, l->seq[i][0], &liste_processus);
+				}
+			}
 		}
 	}
 
